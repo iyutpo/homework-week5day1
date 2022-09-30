fc68195af7d0a38f1e2c47932c6a2b4d09759279
Yinghai Yu <Y15653862659@outlook.com>
2022-09-29 21:18:33 -0700

    second

---
+++ js/activity.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $('.available').click(function(event) {
+        var bkcolor = $(this).css('background-color');
+        console.log('bkcolor: ' + bkcolor);
+        if (bkcolor === 'rgb(0, 255, 0)') {
+            $(this).css({'background-color': '#eeffe6'});
+        } else {
+            $(this).css({'background-color': '#00ff00'});
+        }
+        event.stopPropagation();
+    });
+});
